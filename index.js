84d5513d266b31d608f679743b6429c402740e6b
Kayla Himmelberger <kayla.himmelberger@willowtreeapps.com>
2023-03-28 09:22:28 -0700

    project setup: added github copilot and tested it out in the js and html files

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const bodyId = document.getElementById('root');
+const heroSectionId = document.getElementById('hero');
+
+const createHero = () => {
+  const hero = document.createElement('div');
+  hero.innerHTML = `
+    <h1>Hero</h1>
+    <button>Click me</button>
+    <p>Paragraph</p>
+    <nav>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </nav>
+  `;
+
+  return hero;
+};
+
+heroSectionId.appendChild(createHero());
