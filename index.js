d4e752c477fb51b486b3e4ed0d86b8d477965930
Kayla Himmelberger <kayla.himmelberger@willowtreeapps.com>
2023-03-30 09:16:16 -0700

    AI-3: WIP on accessibly splitting the h1 on mobile

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-const heroSectionId = document.getElementById('hero');
+const heroH1Id = document.getElementById('heroH1');
 
-const createHero = () => {
-  const hero = document.createElement('div');
-  hero.innerHTML = `
-    <h1>Hero</h1>
-    <button>Click me</button>
-    <p>Paragraph</p>
-    <nav>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
-    </nav>
-  `;
-
-  return hero;
-};
-
-// heroSectionId.appendChild(createHero());
