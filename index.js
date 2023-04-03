6f88ed4ea25bacb5919ff27b5d40280c08fb34b3
Christina Peebles <chrpeebles@gmail.com>
2023-04-03 14:37:47 -0700

    AI-25: Added click event listener to add active link styling to site nav links, cleaned up nav css

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// SITE NAVIGATION
+const siteNavLinks = document.querySelectorAll('.site-nav-links a');
+const clearActiveLinkStyles = () => {
+  siteNavLinks.forEach((link) => link.classList.remove('-active'));
+};
+const addActiveLinkStyles = (activeLink) => {
+  activeLink.classList.add('-active');
+};
+
+siteNavLinks.forEach((link) =>
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    clearActiveLinkStyles();
+    addActiveLinkStyles(link);
+
+    window.location.href = link.getAttribute('href');
+  })
+);
+
 // SOCIAL NAVIGATION
 const socialLinks = document.querySelectorAll('.social-nav a');
 
@@ -25,3 +45,12 @@ socialLinks.forEach((link) => {
     link.addEventListener(event, () => onSocialLinkBlur());
   });
 });
+
+// HAMBURGER MENU
+const openHamburgerMenu = () => {
+  console.log('open hamburger menu');
+};
+
+const closeHamburgerMenu = () => {
+  console.log('close hamburger menu');
+};
