523f6a73234094c5ca5aa7c5002dea917137f8d7
Christina Peebles <chrpeebles@gmail.com>
2023-04-03 13:45:42 -0700

    AI-25: Added javascript for social link focus and mouseover event styles

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const heroH1Id = document.getElementById('heroH1');
+// SOCIAL NAVIGATION
+const socialLinks = document.querySelectorAll('.social-nav a');
 
+const onSocialLinkFocus = (focusedLink) => {
+  socialLinks.forEach((link) => {
+    link.style.opacity = 0.25;
+  });
+  focusedLink.style.opacity = 1;
+};
+
+socialLinks.forEach((link) => {
+  ['focus', 'mouseover'].forEach((event) => {
+    link.addEventListener(event, () => onSocialLinkFocus(link));
+  });
+});
+
+const onSocialLinkBlur = () => {
+  socialLinks.forEach((link) => {
+    link.style.opacity = 1;
+  });
+};
+
+socialLinks.forEach((link) => {
+  ['blur', 'mouseout'].forEach((event) => {
+    link.addEventListener(event, () => onSocialLinkBlur());
+  });
+});
