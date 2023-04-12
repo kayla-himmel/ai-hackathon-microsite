10e58cb3d106dbc450d0b2d5849421b490853e22
Kayla Himmelberger <kayla.himmelberger@willowtreeapps.com>
2023-04-11 17:08:58 -0700

    AI-10: WIP mobile fixes for Safari

--- index.js
+++ index.js
@@ -97,3 +97,44 @@ const copyrightYearElement = document.getElementById('copyrightYear');
 const currentYear = new Date().getFullYear();
 
 copyrightYearElement.innerHTML = currentYear;
+
+// FAQ ACCORDION
+const faqContent1 = document.getElementById('faq-1');
+const faqContent2 = document.getElementById('faq-2');
+const faqContent3 = document.getElementById('faq-3');
+const faqContent4 = document.getElementById('faq-4');
+const faqContent5 = document.getElementById('faq-5');
+const faqContent6 = document.getElementById('faq-6');
+const faqContent7 = document.getElementById('faq-7');
+const faqContent8 = document.getElementById('faq-8');
+const faqContent9 = document.getElementById('faq-9');
+const faqContent10 = document.getElementById('faq-10');
+const faqContent11 = document.getElementById('faq-11');
+const faqContent12 = document.getElementById('faq-12');
+const faqContent13 = document.getElementById('faq-13');
+const faqContent14 = document.getElementById('faq-14');
+const faqContent15 = document.getElementById('faq-15');
+const faqContent16 = document.getElementById('faq-16');
+const faqDetailsArray = document.getElementsByClassName('faq-details');
+
+// const isOpen = document
+// click event toggle on summary element that changes the "+" to a "-" and vice versa
+// faqDetailsArray.forEach((li) => {
+// // check if faqContent has open attribute
+// li.addEventListener('click', () => {
+//   // if so, show - button
+//   // else, show + button
+// });
+
+// const onFaqContentClick = (button) => {
+//   socialLinks.forEach((link) => {
+//     link.style.opacity = 0.25;
+//   });
+//   button.style.opacity = 1;
+// };
+
+// socialLinks.forEach((link) => {
+//   ['focus', 'mouseover'].forEach((event) => {
+//     link.addEventListener(event, () => onSocialLinkFocus(link));
+//   });
+// });
