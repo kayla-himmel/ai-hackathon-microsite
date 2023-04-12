cef5e8e8e5018c243157afdbb35e65a273acce21
Kayla Himmelberger <kayla.himmelberger@willowtreeapps.com>
2023-04-12 13:11:28 -0700

    AI-10: revamped accordion buttons and styling, WIP button logic

--- index.js
+++ index.js
@@ -117,26 +117,38 @@ const faqContent13 = document.getElementById('faq-13');
 const faqContent14 = document.getElementById('faq-14');
 const faqContent15 = document.getElementById('faq-15');
 const faqContent16 = document.getElementById('faq-16');
-const faqDetailsArray = document.getElementsByClassName('faq-details');
+const faqLiArray = document.getElementsByClassName('faq-details');
+const faqToggleButtonArray = document.querySelectorAll('.faq-toggle');
+const accordionButtonArray = document.getElementsByClassName('plus');
 
-// const isOpen = document
+accordionButtonArray.forEach((button) => {
+  button.addEventListener('click', () => {
+    details.setArrtibute('open', 'open');
+  });
+});
+
 // click event toggle on summary element that changes the "+" to a "-" and vice versa
-// faqDetailsArray.forEach((li) => {
-// // check if faqContent has open attribute
-// li.addEventListener('click', () => {
-//   // if so, show - button
-//   // else, show + button
-// });
+// change the button when anywhere on the FAQ is clicked
+// const onFaqContentClick = (content) => {
+//   for (let i = 0; i < faqLiArray.length; i++) {
+//     const li = faqLiArray[i];
+//     // check if faqContent has open attribute
+//     li.addEventListener('click', () => {
+//       // if so, show - button
+//       document.documentElement.style.setProperty('--color', 'red');
+//       // define close button after style, then display none
+//       // ___.style.display = 'none';
+//       // else, show + button
+//     });
+//   }
+// };
 
-// const onFaqContentClick = (button) => {
-//   socialLinks.forEach((link) => {
-//     link.style.opacity = 0.25;
+// change the button when the actual button is clicked
+// const toggleAccordion = (faq) => {
+//   faqToggleButtonArray.forEach((button) => {
+//     plusSpanInButton.addEventListener('click', () => {
+//       button.style.display = 0.25;
+//     });
 //   });
 //   button.style.opacity = 1;
 // };
-
-// socialLinks.forEach((link) => {
-//   ['focus', 'mouseover'].forEach((event) => {
-//     link.addEventListener(event, () => onSocialLinkFocus(link));
-//   });
-// });
